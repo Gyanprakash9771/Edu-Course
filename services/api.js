aaf6a12e6d83dcd4173d35b806594e37aab48ddf
Gyanprakash9771 <gyanprakash9771@gmail.com>
2026-04-06 12:24:51 +0530

    added login signup

--- services/api.js
+++ services/api.js
@@ -3,13 +3,14 @@ import { getToken } from "../utils/storage";
 
 const API = axios.create({
   baseURL: "https://wuthering-lai-patently.ngrok-free.dev/api",
+  withCredentials: false,
 });
 
 API.interceptors.request.use(async (req) => {
   const token = await getToken();
 
   if (token) {
-    req.headers.authorization = token;
+    req.headers.Authorization = `Bearer ${token}`;
   }
 
   return req;
